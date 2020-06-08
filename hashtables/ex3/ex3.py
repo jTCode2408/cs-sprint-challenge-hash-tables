3010e59de9db3463ba82f7def5e99736dd779a9d
Jasmine Terry <jasmine-terry91@gmail.com>
2020-06-07 23:34:49 -0400

    ex3 done

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,28 @@
+"""
+numbers that exist in all lists.
+* There can be up to 10 lists in the list of lists.
+* The lists can contain up to roughly 1,000,000 elements each.
+"""
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    #dict for vals
+    #needd to loop over arrays, & each array in arrays
+    #need to keep count of elms in arr of arrays
+    # loop over each array in arrays
+    #check if val is in dict
+    #return: vals that are in arrays
+    dict ={}
+    result = []
+
+    for arr in arrays:
+        for i in arr:
+            if i in dict:
+                dict[i] +=1
+            else:
+                dict[i] =1
+    
+    for (key, value) in dict.items():
+        if value == len(arrays):
+            result.append(key)
 
     return result
 
